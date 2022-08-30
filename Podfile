56efb662d2d3da0dee704a6d3704ceb28693bb7f
jitao.gu <jitao.gu@metalpha.finance>
2022-08-30 17:07:42 +0800

    add project

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'ZendeskDemo' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  
+  pod 'ZendeskSupportSDK'
+  pod 'ZendeskSupportProvidersSDK'
+  pod 'ZendeskChatSDK'
+  pod 'ZendeskChatProvidersSDK'
+  pod 'ZendeskAnswerBotProvidersSDK'
+  pod 'ZendeskAnswerBotSDK'
+  # Pods for ZendeskDemo
+
+end
--- ZendeskDemo/ViewController.m
+++ ZendeskDemo/ViewController.m
@@ -6,6 +6,17 @@
 //
 
 #import "ViewController.h"
+#import <ZendeskCoreSDK/ZendeskCoreSDK.h>
+#import <SupportSDK/SupportSDK.h>
+#import <SupportProvidersSDK/SupportProvidersSDK.h>
+#import <ZendeskCoreSDK/ZendeskCoreSDK-Swift.h>
+#import <MessagingAPI/MessagingAPI.h>
+#import <MessagingSDK/MessagingSDK.h>
+#import <ChatProvidersSDK/ChatProvidersSDK.h>
+#import <AnswerBotProvidersSDK/AnswerBotProvidersSDK.h>
+#import <AnswerBotSDK/AnswerBotSDK.h>
+#import <ChatSDK/ChatSDK.h>
+#import <CommonUISDK/CommonUISDK.h>
 
 @interface ViewController ()
 
@@ -16,6 +27,57 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+}
+
+- (IBAction)initClicked:(id)sender{
+    
+    [ZDKZendesk initializeWithAppId: @"Your AppID"
+        clientId: @"Your ClientID"
+        zendeskUrl: @"Your url"];
+    
+    [ZDKMessaging initialize];
+    [ZDKSupport initializeWithZendesk: [ZDKZendesk instance]];
+    
+    [ZDKAnswerBot initializeWithZendesk:[ZDKZendesk instance] support:[ZDKSupport instance]];
+    [ZDKChat initializeWithAccountKey: @"Your Account Key" queue: dispatch_get_main_queue()];
+    
+}
+
+
+- (IBAction)identityButtonClicked:(id)sender{
+    
+    
+    NSString *email = @"taojigu@163.com";
+    id<ZDKObjCIdentity> userIdentity = [[ZDKObjCAnonymous alloc] initWithName:email email:email];
+    [[ZDKZendesk instance] setIdentity:userIdentity];
+    
+
+    
+}
+
+-(IBAction)pushMessageClicked:(id)sender{
+    UIViewController *vc = [self conversationViewController];
+    [self.navigationController pushViewController:vc animated:NO];
+}
+
+
+- (UIViewController *) conversationViewController {
+
+    
+    ZDKMessagingConfiguration *messagingConfiguration = [ZDKMessagingConfiguration new];
+    NSError *error = nil;
+    
+    NSArray *engines = @[
+        
+        (id <ZDKEngine>) [ZDKAnswerBotEngine engineAndReturnError:&error],
+        (id <ZDKEngine>) [ZDKSupportEngine engineAndReturnError:&error],
+        (id <ZDKEngine>) [ZDKChatEngine engineAndReturnError:&error]
+        
+    ];
+
+    return [[ZDKMessaging instance] buildUIWithEngines:engines
+                                               configs:@[messagingConfiguration]
+                                                 error:&error];
 }
 
 
